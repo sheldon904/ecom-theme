7085e21dfc1e18cae240299beb8c24c57e2dc66c
sheldon904 <128750711+sheldon904@users.noreply.github.com>
2025-04-14 16:51:55 -0400

    cleaned up moved to dev site

--- lambo_merch_theme/homepage.php
+++ lambo_merch_theme/homepage.php
@@ -16,14 +16,14 @@ get_header();
     <div class="container-fluid p-0">
         <div class="row no-gutters">
             <div class="col-12 text-center">
-                <img src="http://ecom-test.local/wp-content/uploads/2025/04/Big_LM_logo.png" alt="Lamborghini Merch Logo" class="img-fluid" />
+                <img src="http://lambomerch.madefreshdev.cloud/wp-content/uploads/2025/04/Big_LM_logo.png" alt="Lamborghini Merch Logo" class="img-fluid" />
                 <h1 style="font-family: 'Georgia', serif; font-style: italic;">Luxury Merch for Lambo Enthusiasts</h1>
             </div>
             
             <div class="col-12 text-center position-relative">
                 <!-- T-shirt box with SHOP NOW button overlay -->
                 <div class="shop-box-container">
-                    <img src="http://ecom-test.local/wp-content/uploads/2025/04/Shop_Box.png" alt="Lamborghini T-shirts for sale shop box" class="img-responsive centerimage" />
+                    <img src="http://lambomerch.madefreshdev.cloud/wp-content/uploads/2025/04/Shop_Box.png" alt="Lamborghini T-shirts for sale shop box" class="img-responsive centerimage" />
                     <a href="/shop" class="shop-now-button">SHOP NOW</a>
                 </div>
             </div>
@@ -31,7 +31,7 @@ get_header();
             <div class="col-12 p-0">
                 <!-- Video image that stays centered -->
                 <div class="video-container">
-                    <img src="http://ecom-test.local/wp-content/uploads/2025/04/Video.png" alt="Lamborghini Video" />
+                    <img src="http://lambomerch.madefreshdev.cloud/wp-content/uploads/2025/04/Video.png" alt="Lamborghini Video" />
                 </div>
             </div>
         </div>
